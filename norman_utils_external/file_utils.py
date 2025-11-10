cb36ff7e5116492b0110c6eb91b3d8b07095c293
Dolev <dolev.gabay@norman-ai.com>
2025-11-10 18:58:15 +0200

    Changed the order of the checks in get buffer size.
    (#86c4p1j6d)

--- norman_utils_external/file_utils.py
+++ norman_utils_external/file_utils.py
@@ -12,10 +12,10 @@ class FileUtils(metaclass=Singleton):
 
     @staticmethod
     def get_buffer_size(file_obj):
-        if hasattr(file_obj, "fileno"):
-            return os.fstat(file_obj.fileno()).st_size
         if isinstance(file_obj, io.BytesIO):
             return file_obj.getbuffer().nbytes
+        if hasattr(file_obj, "fileno"):
+            return os.fstat(file_obj.fileno()).st_size
         raise ValueError("Unsupported file object or operation")
 
     def get_file_type(self, file_path: str):
