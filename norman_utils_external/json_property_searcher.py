9de80d8c4b1c1347615b02a978ff2b7cf7f7f8be
AmitMizrachi <amit.mizrachi@norman-ai.com>
2025-12-16 02:36:34 +0200

    Adding union of set lisst and tuple, adding set to isinstance, extra line of space. Adding union of set lisst and tuple, adding set to isinstance, extra line of space.
    (#86c5cuvtx)

--- norman_utils_external/json_property_searcher.py
+++ norman_utils_external/json_property_searcher.py
@@ -1,25 +1,27 @@
 from collections import deque
+from typing import Union
 
 
 class JsonPropertySearcher:
 
     @staticmethod
-    def search(source: dict, keys: set):
+    def search(source: dict, keys: Union[set, list, tuple]):
         results = {}
         remaining_keys = set(keys)
         queue = deque([source])
 
         while len(queue) > 0 and len(remaining_keys) > 0:
-            current = queue.popleft()
+            current_node = queue.popleft()
 
-            if isinstance(current, dict):
-                for key, value in current.items():
+            if isinstance(current_node, dict):
+                for key, value in current_node.items():
                     if key in remaining_keys:
                         results[key] = value
                         remaining_keys.discard(key)
+
                     queue.append(value)
 
-            elif isinstance(current, (list, tuple)):
-                queue.extend(current)
+            elif isinstance(current_node, (set, list, tuple)):
+                queue.extend(current_node)
 
         return results
