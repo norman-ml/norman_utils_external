0a612f421fc8f740e855b5aa9101ecadf497d4a4
AmitMizrachi <amit.mizrachi@norman-ai.com>
2025-12-24 19:36:07 +0200

    Changing from double ifs to if and elif, removing the repetitive one that we've had. Changing from double ifs to if and elif, removing the repetitive one that we've had.
    (#86c77fv4u)

--- norman_utils_external/file_utils.py
+++ norman_utils_external/file_utils.py
@@ -14,10 +14,8 @@ class FileUtils(metaclass=Singleton):
     def get_buffer_size(file_obj):
         if isinstance(file_obj, io.BytesIO):
             return file_obj.getbuffer().nbytes
-        if hasattr(file_obj, "fileno"):
+        elif hasattr(file_obj, "fileno"):
             return os.fstat(file_obj.fileno()).st_size
-        if isinstance(file_obj, io.BytesIO):
-            return file_obj.getbuffer().nbytes
         raise ValueError("Unsupported file object or operation")
 
     def get_file_type(self, file_path: str):
