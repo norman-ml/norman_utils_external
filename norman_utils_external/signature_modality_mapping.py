b136e82b33c716c4cb2898943720b3c0ab8b0d7c
ori-small <ori.small@norman-ai.com>
2026-01-07 20:09:58 +0200

    CR fixes: added zip mapping for file recognizing in sanitization checks. CR fixes: added zip mapping for file recognizing in sanitization checks.
    (#86bx11g42)

--- norman_utils_external/signature_modality_mapping.py
+++ norman_utils_external/signature_modality_mapping.py
@@ -31,5 +31,6 @@ class SignatureModalityMapping:
 
             # File (container level)
             "jit": "File",
-            "pt": "File"
+            "pt": "File",
+            "zip": "File"
         }
