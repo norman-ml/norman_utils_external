3b275cef48b89effc0e2d7f1d7e2aa473ce52e1f
ori-small <ori.small@norman-ai.com>
2026-01-07 15:14:21 +0200

    CR fixes: split file type categories to mapping and helper functions. CR fixes: split file type categories to mapping and helper functions.
    (#86bx11g42)

--- norman_utils_external/sanitization_utils/file_sanitizer.py
+++ norman_utils_external/sanitization_utils/file_sanitizer.py
@@ -2,16 +2,17 @@ from typing import Optional, Set
 
 import filetype
 
-from norman_utils_external.sanitization_utils.file_type_categories import FileTypeCategories
+from norman_utils_external.sanitization_utils.file_type_helpers import FileTypeHelpers
 
 
 class FileSanitizer:
 
+    # The number of bytes required by filetype package for type resolving
     MIN_BYTES_FOR_DETECTION = 261
 
     @classmethod
     def sanitize_file_bytes(cls, file_content: bytes, allowed_file_types: Set[str]):
-        if FileTypeCategories.has_text_based_type(allowed_file_types):
+        if FileTypeHelpers.has_text_based_type(allowed_file_types):
             return None
 
         if len(file_content) < cls.MIN_BYTES_FOR_DETECTION:
@@ -32,9 +33,9 @@ class FileSanitizer:
         if cls.__is_allowed_container_type(detected_type, allowed_file_types):
             return detected_type
 
-        normalized_detected = FileTypeCategories.normalize_type(detected_type)
+        normalized_detected = FileTypeHelpers.normalize_type(detected_type)
         normalized_allowed = {
-            FileTypeCategories.normalize_type(ft) for ft in allowed_file_types
+            FileTypeHelpers.normalize_type(ft) for ft in allowed_file_types
         }
 
         if normalized_detected not in normalized_allowed:
@@ -47,13 +48,13 @@ class FileSanitizer:
 
     @classmethod
     def __is_allowed_container_type(cls, detected_type: str, allowed_file_types: Set[str]):
-        if detected_type == "zip" and FileTypeCategories.has_zip_based_type(allowed_file_types):
+        if detected_type == "zip" and FileTypeHelpers.has_zip_based_type(allowed_file_types):
             return True
 
-        if detected_type == "webm" and FileTypeCategories.has_matroska_type(allowed_file_types):
+        if detected_type == "webm" and FileTypeHelpers.has_matroska_type(allowed_file_types):
             return True
 
-        if detected_type == "ogg" and FileTypeCategories.has_ogg_type(allowed_file_types):
+        if detected_type == "ogg" and FileTypeHelpers.has_ogg_type(allowed_file_types):
             return True
 
         return False
