7823e2ea812e29ffc8b30ce162d472385a45b813
Yonatan Alon <yonatan.alon@norman-ai.com>
2026-01-14 11:56:00 +0200

    Removed now unused signature modality mapping class.
    (#86c6x1m7k)

--- norman_utils_external/file_sanitizer.py
+++ norman_utils_external/file_sanitizer.py
@@ -1,5 +1,5 @@
+from norman_utils_external.encoding_combinations import EncodingCombinations
 from norman_utils_external.file_utils import FileUtils
-from norman_utils_external.signature_modality_mapping import SignatureModalityMapping
 from norman_utils_external.singleton import Singleton
 
 
@@ -7,11 +7,10 @@ class FileSanitizer(metaclass=Singleton):
     def __init__(self):
         self.__file_utils = FileUtils()
 
-    def sanitize(self, file_extension: str, required_modality: str):
-        if file_extension not in SignatureModalityMapping.Encoding_Map:
-            raise ValueError("File type is not supported.")
+    def sanitize(self, data_modality: str, required_modality: str):
+        if data_modality not in EncodingCombinations.Combinations_Map:
+            raise ValueError("File type is not supported")
 
-        data_modality = SignatureModalityMapping.Encoding_Map[file_extension]
         if data_modality != required_modality:
-            raise ValueError("File modality does not match the expected modality.")
+            raise ValueError("File modality does not match the expected modality")
 
